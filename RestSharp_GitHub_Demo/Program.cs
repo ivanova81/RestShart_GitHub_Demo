9cc283fae3a8a3c20133e8e3c3b7ffb7e88d6b2e
Monica Ivanova <98054187+ivanova81@users.noreply.github.com>
2022-05-24 15:17:42 +0300

    Add files via upload

---
+++ RestSharp_GitHub_Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+var client = new RestClient("https://api.github.com");
+
+client.Authenticator = new HttpBasicAuthenticator("ivanova81", "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");
+
+//var request = new RestRequest("/repos/ivanova81/postman/issues"); 
+
+//var request = new RestRequest("/users/ivanova81/repos");
+
+string url = "/repos/ivanova81/postman/issues";
+
+var request = new RestRequest(url);
+
+request.AddBody(new { title = "New Issue from RestSharp" });
+    ;
+var response = await client.ExecuteAsync(request, Method.Post);
+
+Console.WriteLine("STATUS CODE : " + response.StatusCode);
+Console.WriteLine("BODY : " + response.Content);
+
+//var repos = JsonSerializer.Deserialize<List<Repo>>(response.Content);
+
+//var issues = JsonSerializer.Deserialize<List<Issue>>(response.Content);
+
+//foreach (var issue in issues)
+//{
+//    Console.WriteLine("ISSUE NUMBER : " + issue.number);
+
+//    Console.WriteLine("ISSUE ID : " + issue.id);
+
+//    Console.WriteLine("ISSUE URL : " + issue.html_url);
+
+//    Console.WriteLine("*************");
+//}
+
+//foreach (var repo in repos)
+//{
+//    Console.WriteLine("REPO FULL NAME : " + repo.full_name);
+//}
+
+//Console.WriteLine("STATUS CODE : " + response.StatusCode);
+//Console.WriteLine("BODY : " + response.Content);
+
